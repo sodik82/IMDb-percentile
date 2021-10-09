49759213f3572c61acd1b71578906dab5a51d100
stanislav miklik <stanislav.miklik@gmail.com>
2021-10-09 17:24:34 +0200

    Compute percentile

--- doStatistics.sh
+++ doStatistics.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-set -x
-
 for P in {0..100} ; do
-    docker exec -it imdb-neo4j_neo4j_1 cypher-shell "MATCH (n:Titles) RETURN toFloat($P) / 100, percentileDisc(n.avgRating, toFloat($P) / 100)"
+    docker exec -it imdb-neo4j_neo4j_1 cypher-shell --format plain "MATCH (n:Titles) RETURN toFloat($P) / 100, percentileDisc(n.avgRating, toFloat($P) / 100)"
 done
