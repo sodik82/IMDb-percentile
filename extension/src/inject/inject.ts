7c1d2a78bda1a18ba0508dba87f02f1c8523063d
stanislav miklik <stanislav.miklik@gmail.com>
2021-10-15 23:12:40 +0200

    ability to set percentile

--- extension/src/inject/inject.ts
+++ extension/src/inject/inject.ts
@@ -1,5 +1,5 @@
 import { test } from "./data";
-import { getRating, getTitleId } from "./page-utils";
+import { getRating, getTitleId, setPercentile } from "./page-utils";
 
 console.log("Hello from IMDb percentile");
 var readyStateCheckInterval = setInterval(function () {
@@ -7,11 +7,14 @@ var readyStateCheckInterval = setInterval(function () {
     clearInterval(readyStateCheckInterval);
     start();
   }
-}, 10);
+}, 100);
 
 function start() {
   console.log("Hello. This message was sent from scripts/inject.js");
   const titleId = getTitleId();
   const rating = getRating();
   console.log("percentile info", { titleId, rating });
+  if (rating) {
+    setPercentile(rating * 10);
+  }
 }
--- extension/src/inject/page-utils.ts
+++ extension/src/inject/page-utils.ts
@@ -19,11 +19,28 @@ export function getIdFromUrl(urlPath: string): string | undefined {
 }
 
 export function getRating() {
-    const el = document.querySelector("[data-testid='hero-rating-bar__aggregate-rating__score']")
-    if (!el) {
-        return undefined
-    }
-    // @ts-ignore
-    const text = el.children[0].innerText;
-    return parseFloat(text);
+  const el = getRatingElement();
+  if (!el) {
+    return undefined;
+  }
+  // @ts-ignore
+  const text = el.children[0].innerText;
+  return parseFloat(text);
+}
+
+export function setPercentile(value: number) {
+  const el = getRatingElement()!;
+  const sybling = el?.children[1];
+  if (!sybling) {
+    console.warn("Promile - can't set percentile " + value);
+    return;
+  }
+  console.log('setting', sybling)
+  sybling.innerHTML = `<b>${value}</b>`;
+}
+
+function getRatingElement() {
+  return document.querySelector(
+    "[data-testid='hero-rating-bar__aggregate-rating__score']"
+  );
 }
